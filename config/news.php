cf23ed55cc43d5161edbd468b4b272dd497da014
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2026-03-04 23:28:52 +0100

    setup config

---
+++ config/news.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+return [
+    'newsapi' => [
+        'api_key' => env('NEWS_API_KEY'),
+        'url' => env('NEWSAPI_URL', 'https://api.nytimes.com/svc/search/v2/'),
+    ],
+    'nytimes' => [
+        'api_key' => env('NYTIMES_API_KEY'),
+        'url' => env('NYTIMES_URL', 'https://api.nytimes.com/svc/search/v2/'),
+    ],
+    'guardian' => [
+        'api_key' => env('GUARDIAN_API_KEY'),
+        'url' => env('NYTIMES_URL', 'https://api.nytimes.com/svc/search/v2/'),
+    ],
+];
+
