53abac0774c877e5135e9ca2380b01a9dbf6f220
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2026-03-07 11:58:13 +0100

    article, sorting

---
+++ app/Actions/GetArticlesAction.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Actions;
+
+
+use App\DTOs\ArticleDTO;
+use App\Models\Article;
+use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Contracts\Pagination\Paginator;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+
+class GetArticlesAction
+{
+    public function __invoke(): array|Paginator|CursorPaginator
+    {
+        $query = QueryBuilder::for(Article::class)
+            ->with(['authors', 'categories'])
+            ->allowedFilters([
+                AllowedFilter::partial('title'),
+                AllowedFilter::partial('authors.name'),
+                AllowedFilter::partial('categories.name'),
+                AllowedFilter::exact('source'),
+                AllowedFilter::exact('published_at'),
+            ])
+            ->allowedIncludes('authors', 'categories')
+            ->defaultSort('-published_at')
+            ->allowedSorts(['title', 'authors.name', 'categories.name', 'source', 'published_at']);
+
+
+        return ArticleDTO::collect($query->cursorPaginate());
+    }
+}
--- app/Http/Controllers/FetchArticlesController.php
+++ app/Http/Controllers/FetchArticlesController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\ArticleService;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use App\Actions\GetArticlesAction;
+use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Contracts\Pagination\Paginator;
 
 class FetchArticlesController extends Controller
 {
-    public function __invoke(ArticleService $articleService): AnonymousResourceCollection
+    public function __invoke(GetArticlesAction $action): array|Paginator|CursorPaginator
     {
-        return $articleService->fetchArticles();
+        return $action();
     }
 }
